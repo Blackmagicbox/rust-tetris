017d9adc5acf009ff7623654ed0a7fa5843e3d2b
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-06 01:51:44 +0100

    FEAT: Basic Engine and Interface structs [DRAFT]

---
+++ src/interface/mod.rs
@@ -0,0 +1,11 @@
+use crate::engine::Engine;
+
+pub struct Interface {
+    engine: Engine,
+}
+
+impl Interface {
+    pub fn run() {
+        // Todo ("Run the game")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[allow(dead_code)]
+use crate::engine::Engine;
+
 mod engine;
 mod interface;
 
 fn main() {
+    let engine =  Engine::new();
     println!("Let's play Tetris");
 }
